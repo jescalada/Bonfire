94ebc055b9a487bb5429205ca28f09ed88243b4b
Juan Escalada <juanescalada175@gmail.com>
2022-04-28 10:35:55 -0700

    Refactor mysql package to mysql2 and use pool db

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const mysql = require('mysql')
+const mysql = require('mysql2/promise')
 const { get } = require('express/lib/response')
 
 const app = express()
@@ -9,19 +9,17 @@ const port = 3000
 app.set('view engine', 'ejs');
 
 // Connection info should be obtained using a .env
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
+  connectionLimit: 10,
   host: '35.193.183.129',
   user: 'jescalada',
   password: 'Rocco123',
   database: 'bonfire-db'
 })
 
-connection.connect()
-console.log("Connected!");
-
 function createTable() {
   var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
-  connection.query(sql, function (err, result) {
+  pool.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table customers created");
   });
@@ -30,17 +28,10 @@ function createTable() {
 function populateCustomers() {
   let randomNum = Math.floor(Math.random() * 1000);
   var sql = `INSERT INTO customers (name, address) values ('Juan${randomNum}', '${randomNum} Testing St');`;
-  connection.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("Inserted some data!");
-  });
+  pool.query(sql);
 }
 
 populateCustomers()
-// createTable()
-
-// app.use(express.static('public')); // Tells express that there's a directory called public
-// app.use(express.static('models')); // Tells express that there's a directory called models
 
 app.get('/', (req, res) => {
   let queryRows
