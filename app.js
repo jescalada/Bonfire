646f677f06a7cc6e6ebb9f24e5a879dec694b99b
Juan Escalada <juanescalada175@gmail.com>
2022-05-02 16:27:38 -0700

    Add bcrypt package and simple user registration

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express')
 const path = require('path')
 const mysql = require('mysql2/promise')
 const { get } = require('express/lib/response')
+const bcrypt = require('bcrypt')
 
 const app = express()
 const port = 3000
 
+const users = [] // We put the users in here temporarily, later we integrate it to the database
+
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false })) // Tells our application to take the forms and access them from the request object
 
 // Connection info should be obtained using a .env
 const pool = mysql.createPool({
@@ -43,24 +47,37 @@ app.get('/', (req, res) => {
   })
 })
 
-// Serves the login page
+// GET login page
 app.get('/login', (req, res) => {
-  getRows().then(function ([rows, fields]) {
-    res.render('pages/login', {
-      query: rows,
-      test: "testing"
-    });
-  })
+  res.render('pages/login')
 })
 
-// Serves the registration page
+// POST login page
+app.post('/login', (req, res) => {
+  
+})
+
+// GET registration page
 app.get('/register', (req, res) => {
-  getRows().then(function ([rows, fields]) {
-    res.render('pages/register', {
-      query: rows,
-      test: "testing"
-    });
-  })
+  res.render('pages/register');
+})
+
+// POST login page
+app.post('/register', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword
+    })
+    res.redirect('/login') // Redirect to login page on success
+  } catch(err) {
+    console.log(err);
+    res.redirect('/register') // Return to registration page on failure
+  }
+  console.log(users)
 })
 
 async function getRows() {
