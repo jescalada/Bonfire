cbd224ca052efed95cf75507c216d8764f7024d9
Juan Escalada <juanescalada175@gmail.com>
2022-05-24 10:19:24 -0700

    Document passport-config.js

--- scripts/passport-config.js
+++ scripts/passport-config.js
@@ -1,6 +1,14 @@
+// Script containing configuration for passport.js
+
+// Initialize a Local Strategy for authentication (in this case, using our own email and userId to auth)
 const LocalStrategy = require('passport-local').Strategy
+
+// Uses a password hashing library
 const bcrypt = require('bcryptjs')
 
+// Initializes the passport object with various functionalities for authenticating
+// Takes a passport object, a function that allows getting a user by email, and a function that allows getting a user by ID
+// Returns an initialized passport object with various authentication functions
 function initialize(passport, getUserByEmail, getUserById) {
     const authenticateUser = async (email, password, done) => {
         let user = await getUserByEmail(email)
@@ -21,8 +29,9 @@ function initialize(passport, getUserByEmail, getUserById) {
             return done(e)
         }
     }
-
+    // Uses the email field as the UNIQUE identifier for authentication
     passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
+    // Provides user serialization and deserialization functions
     passport.serializeUser((user, done) => {
         done(null, user.user_id)
     })
@@ -31,4 +40,5 @@ function initialize(passport, getUserByEmail, getUserById) {
     })
 }
 
-module.exports = initialize //exports the initialize function
+//exports the initialize function
+module.exports = initialize
